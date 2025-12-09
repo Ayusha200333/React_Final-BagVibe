62710f51c33fa48bfaf42f5532dff204f48b5a72
Ayusha200333 <ayushawijerathna@gmail.com>
2025-12-09 21:01:54 +0530

    add product section

--- src/components/Layout/Hero.tsx
+++ src/components/Layout/Hero.tsx
@@ -1,7 +1,45 @@
+// import React from 'react'
+// import heroImg from "../../assets/herolmg-jpg.webp";
+// import { Link } from 'react-router-dom';
+
+
+// const Hero = () => {
+//   return (
+//    <section className="relative">
+//       <img
+//         src={heroImg}
+//         alt="BagVibe"
+//         className="w-full h-[200px] md:h-[300px] lg:h-[500px] object-cover"
+//       />
+
+//       <div className="absolute inset-0 bg-black/40 flex items-center justify-center">
+//         <div className="text-center text-white p-6">
+//           <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold tracking-tighter uppercase">
+//             Bag Away
+//           </h1>
+
+//           <p className='text-sm tracking-tighter md:text-lg mb-6'>
+//                 Stylish bags for every journey  - comfort, quality, and adventure.
+//           </p>
+
+//           <Link to="#" className='bg-white text-gray-950 px-6 py-2 rounded-sm text-lg'>
+//             Go Explore
+//           </Link>
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Hero
+
+
+
+
+
 import React from 'react'
 import heroImg from "../../assets/herolmg-jpg.webp";
 import { Link } from 'react-router-dom';
-
 
 const Hero = () => {
   return (
@@ -14,15 +52,15 @@ const Hero = () => {
 
       <div className="absolute inset-0 bg-black/40 flex items-center justify-center">
         <div className="text-center text-white p-6">
-          <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold tracking-tighter uppercase">
+          <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold tracking-tighter uppercase opacity-0 animate-fadeInDown">
             Bag Away
           </h1>
 
-          <p className='text-sm tracking-tighter md:text-lg mb-6'>
-                Stylish bags for every journey  - comfort, quality, and adventure.
+          <p className='text-sm tracking-tighter md:text-lg mb-6 mt-4 opacity-0 animate-fadeInUp delay-200'>
+                Stylish bags for every journey - comfort, quality, and adventure.
           </p>
 
-          <Link to="#" className='bg-white text-gray-950 px-6 py-2 rounded-sm text-lg'>
+          <Link to="#" className='bg-white text-gray-950 px-6 py-2 rounded-sm text-lg mt-2 inline-block opacity-0 animate-fadeInUp delay-400'>
             Go Explore
           </Link>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
+import MenCollection from '../components/Products/MenCollection'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
+        <MenCollection/>
     </div>
   )
 }

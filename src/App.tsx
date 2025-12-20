a727a9d9e5e0d137f346ab2cd8fe6c08d8c65d0e
Ayusha200333 <ayushawijerathna@gmail.com>
2025-12-20 16:53:35 +0530

    add order management page

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import AdminHomePage from './pages/AdminHomePage'
 import UserManagement from './components/Admin/UserManagement'
 import ProductManagement from './components/Admin/ProductManagement'
 import EditProductPage from './components/Admin/EditProductPage'
+import OrderManagement from './components/Admin/OrderManagement'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -45,6 +46,7 @@ function App() {
         <Route path="users" element={<UserManagement/>}/>
         <Route path="products" element={<ProductManagement/>}/>
         <Route path="products/:id/edit" element={<EditProductPage/>}/>
+        <Route path="orders" element={<OrderManagement/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
